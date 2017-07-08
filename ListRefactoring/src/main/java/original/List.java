1dff229cba5d756acf73b9983e0117c283801130
sejingo <sejinhome@gmail.com>
2017-07-08 18:58:38 +0900

    magic number extract to constant

--- ListRefactoring/src/main/java/original/List.java
+++ ListRefactoring/src/main/java/original/List.java
@@ -1,16 +1,19 @@
 package original;
 
 public class List {
-	private Object[] elements = new Object[10];
+	private static final int INIT_SIZE = 10;
+	private static final int INCREASE_SIZE = 10;
+
+	private Object[] elements = new Object[INIT_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
 		if (!readOnly) {
 			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
+
+			if (newSize > elements.length) {
+				Object[] newElements = new Object[elements.length + INCREASE_SIZE];
 				for (int i = 0; i < size; i++) {
 					newElements[i] = elements[i];
 				}
